9ba47f418a68f4ce4f90002f78cfc0b35a72a6a2
Andy Morales <andyemmejia@gmail.com>
2023-03-08 14:17:21 -0500

    Ejercicio de Ecuación Cuadrática.

---
+++ EcuacionCuadratica/EcuacionCuadratica/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EcuacionCuadratica
+{
+    public class EcuacionCuadratica
+    {
+        static void Main(string[] args)
+        {
+            double result1, result2, a, b, c;
+
+            Console.Write("Por favor ingrese un valor para a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Por favor ingrese un valor para b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Por favor ingrese un valor para c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            result1 = (((-1) * b) + Math.Sqrt(((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
+
+            result2 = (((-1) * b) - Math.Sqrt(((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
+
+
+            Console.WriteLine("El resultado + de la ecuación cuadrática es:  " + result1);
+            Console.WriteLine("El resultado - de la ecuación cuadrática es:  " + result2);
+        }
+    }
+}
